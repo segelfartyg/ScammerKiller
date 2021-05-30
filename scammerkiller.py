ac07c45605f52ee88f400152f05077d6018c6885
segelfartyg <samuel.swaren@outlook.com>
2021-05-30 14:53:15 +0200

    removed redirect

--- scammerkiller.py
+++ scammerkiller.py
@@ -3,6 +3,12 @@ import os
 import random
 import string
 import json
+from fake_useragent import UserAgent
+
+ua = UserAgent();
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
 
 chars = string.ascii_letters + string.digits + "!@#$%^&*()"
 numbers = "12345678"
@@ -18,7 +24,6 @@ users = json.loads(open("users.json").read())
 passwords = json.loads(open("passwords.json").read())
 
 
-
 for user in users:
     name_extra = "".join(random.choice(string.digits))
     # username = name.lower() + name_extra + "@google.com"
@@ -26,10 +31,11 @@ for user in users:
     # password = random.choice(animals) + "".join(random.choice(numbers) for i in range(8))
     password = random.choice(passwords)
 
-    requests.post(url, allow_redirects=False, data={
+    result = requests.post(url, headers=headers, data={
         "login": username,
         "password": password
     })
+    print(result)
     print('sending %s username and %s password' % (username, password))
 
 
